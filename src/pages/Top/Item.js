3d41582a032410ad338b9f84f3b86a8299dd4652
Bokyung Kim <kbkllmlk17@gmail.com>
2022-06-25 00:44:45 +0900

    top20 UI

---
+++ src/pages/Top/Item.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import css from './Item.module.scss';
+
+function Item(props) {
+  return (
+    <div className={css.block}>
+      <div className={css.left}>
+        <div className={css.rankBlock}>{props.rank}</div>
+        <div className={css.nameBlock}>{props.name}</div>
+      </div>
+
+      <div className={css.typeBlock}>{props.type}</div>
+
+      <div className={css.imageBox}>
+        <img className={css.imagesBlock} src={props.image1}></img>
+        <img className={css.imagesBlock} src={props.image2}></img>
+        {/* <img className={css.imagesBlock} src={props.image3}></img> */}
+      </div>
+    </div>
+  );
+}
+
+export default Item;
