720288b5286b3fe80dbe25c997fbef7e3014884e
Bokyung Kim <kbkllmlk17@gmail.com>
2022-06-27 15:47:29 +0900

    collection ui added

--- src/pages/Collections/Block.js
+++ src/pages/Collections/Block.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import css from './Block.module.scss';
+import { Link } from 'react-router-dom';
 
 function Block(props) {
   return (
-    <div className={css.block}>
+    <Link to="/collections" className={css.block}>
       <img className={css.image} src={props.image}></img>
       <div className={css.subimages}>
-        <img className={css.subimage} src={props.subimages}></img>
-        <img className={css.subimage} src={props.subimages2}></img>
+        <img className={css.subimage1} src={props.image}></img>
+        <img className={css.subimage2} src={props.subimages2}></img>
       </div>
       <div className={css.name}>{props.name}</div>
       <div className={css.price}>₩ {props.price}</div>
-    </div>
+    </Link>
   );
 }
 
--- src/pages/Collections/Collections.js
+++ src/pages/Collections/Collections.js
@@ -1,95 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import css from './Collections.module.scss';
 import Block from './Block';
 
 function Collections() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:10010/collection', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setItems(...items, data);
+      });
+  }, []);
+
   return (
     <div className={css.container}>
-      <Block
-        name="T-Light Jacket"
-        price="99,000"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
-      <Block
-        name="T-Light Jacket"
-        subimages="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Black1_1080x.jpg?v=1648110274"
-        price="99,000"
-        image="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-        subimages2="https://cdn.shopify.com/s/files/1/0562/4971/2815/products/T-Light-Jacket-Ivory1_1080x.jpg?v=1648110273"
-      />
+      {items.map(item => (
+        <Block
+          name={item.productName}
+          price={item.price}
+          type={item.categoryId}
+          image={item.colorImage[0].images[0].url}
+          subimages2={item.colorImage[1].images[0].url}
+        />
+      ))}
     </div>
   );
 }
