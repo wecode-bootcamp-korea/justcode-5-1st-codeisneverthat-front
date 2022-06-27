e5efe6c42ae5c2300d8397571632061d914edace
Sujin Choi <hissujinc@gmail.com>
2022-06-27 17:15:58 +0900

    Fix: Search.module.scss 수정, modal '.dim' 위치 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,43 +6,38 @@ import css from './Login.module.scss';
 
 function Login() {
   return (
-    <>
-      <Header />
-      <div id={css['login']}>
-        <form className={css.cont}>
-          <div className={css.cnt_div}>
-            <label htmlFor={css.name}>이메일</label>
-            <input
-              id="name"
-              type="text"
-              placeholder="이메일"
-              autoComplete="off"
-            />
-          </div>
-          <div className={css.cnt_div}>
-            <label htmlFor="email">비밀번호</label>
-            <input
-              id="email"
-              type="password"
-              placeholder="비밀번호"
-              autoComplete="off"
-            />
-          </div>
+    <div id={css['login']}>
+      <form className={css.cont}>
+        <div className={css.cnt_div}>
+          <label htmlFor={css.name}>이메일</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="이메일"
+            autoComplete="off"
+          />
+        </div>
+        <div className={css.cnt_div}>
+          <label htmlFor="email">비밀번호</label>
+          <input
+            id="email"
+            type="password"
+            placeholder="비밀번호"
+            autoComplete="off"
+          />
+        </div>
 
-          <button className={css.btn_black}>LOGIN</button>
-          <p className={css.text}>
-            이메일, 비밀번호만 설정 후 회원가입 하시면 상품 결제, 주문 확인 및
-            배송 조회, 적립금 혜택 등 더욱 편리하게 스토어를 이용하실 수
-            있습니다.
-          </p>
-          <Link to="/signup" className={css.btn_goLink}>
-            회원가입 하기
-          </Link>
-          <button className={css.btn_goLink}>비밀번호 찾기</button>
-        </form>
-      </div>
-      <Footer />
-    </>
+        <button className={css.btn_black}>LOGIN</button>
+        <p className={css.text}>
+          이메일, 비밀번호만 설정 후 회원가입 하시면 상품 결제, 주문 확인 및
+          배송 조회, 적립금 혜택 등 더욱 편리하게 스토어를 이용하실 수 있습니다.
+        </p>
+        <Link to="/signup" className={css.btn_goLink}>
+          회원가입 하기
+        </Link>
+        <button className={css.btn_goLink}>비밀번호 찾기</button>
+      </form>
+    </div>
   );
 }
 
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -6,37 +6,28 @@ import css from './Signup.module.scss';
 
 function Signup() {
   return (
-    <>
-      <Header />
-      <div id={css['signup']}>
-        <form className={css.cont}>
-          <div className={css.cnt_div}>
-            <label htmlFor={css.name}>이름</label>
-            <input
-              id="name"
-              type="text"
-              placeholder="이름"
-              autoComplete="off"
-            />
-          </div>
-          <div className={css.cnt_div}>
-            <label htmlFor="email">이메일</label>
-            <input
-              id="email"
-              type="text"
-              placeholder="이메일"
-              autoComplete="off"
-            />
-          </div>
+    <div id={css['signup']}>
+      <form className={css.cont}>
+        <div className={css.cnt_div}>
+          <label htmlFor={css.name}>이름</label>
+          <input id="name" type="text" placeholder="이름" autoComplete="off" />
+        </div>
+        <div className={css.cnt_div}>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            type="text"
+            placeholder="이메일"
+            autoComplete="off"
+          />
+        </div>
 
-          <button className={css.btn_black}>CREATE ACCOUNT</button>
-          <Link to="/login" className={css.btn_goLink}>
-            로그인으로 돌아가기
-          </Link>
-        </form>
-      </div>
-      <Footer />
-    </>
+        <button className={css.btn_black}>CREATE ACCOUNT</button>
+        <Link to="/login" className={css.btn_goLink}>
+          로그인으로 돌아가기
+        </Link>
+      </form>
+    </div>
   );
 }
 
