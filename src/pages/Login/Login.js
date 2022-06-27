299e8ba9abef55fef241eba20fa9b089693411fe
Sujin Choi <hissujinc@gmail.com>
2022-06-27 20:36:04 +0900

    Add: 회원가입, 로그인 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,48 +1,96 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import css from './Login.module.scss';
 
 function Login() {
-  return (
-    <>
-      <Header />
-      <div id={css['login']}>
-        <form className={css.cont}>
-          <div className={css.cnt_div}>
-            <label htmlFor={css.name}>이메일</label>
-            <input
-              id="name"
-              type="text"
-              placeholder="이메일"
-              autoComplete="off"
-            />
-          </div>
-          <div className={css.cnt_div}>
-            <label htmlFor="email">비밀번호</label>
-            <input
-              id="email"
-              type="password"
-              placeholder="비밀번호"
-              autoComplete="off"
-            />
-          </div>
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
 
-          <button className={css.btn_black}>LOGIN</button>
-          <p className={css.text}>
-            이메일, 비밀번호만 설정 후 회원가입 하시면 상품 결제, 주문 확인 및
-            배송 조회, 적립금 혜택 등 더욱 편리하게 스토어를 이용하실 수
-            있습니다.
-          </p>
-          <Link to="/signup" className={css.btn_goLink}>
-            회원가입 하기
-          </Link>
-          <button className={css.btn_goLink}>비밀번호 찾기</button>
-        </form>
-      </div>
-      <Footer />
-    </>
+  const handleInputsByName = e => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = event => {
+    event.preventDefault();
+    fetch('http://localhost:10010/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: inputs.email,
+        password: inputs.password,
+      }),
+    })
+      .then(response => {
+        if (response.status === 201) {
+          navigate('/Home');
+        } else {
+          alert('로그인/비밀번호를 확인해주세요');
+        }
+        return response.json();
+      })
+      .then(result => {
+        console.log(result);
+        // localStorage.getItem("token");
+
+        if (result.message.includes('SUCCESS')) {
+          localStorage.setItem('token', result.token);
+        } else {
+          localStorage.setItem('token', '');
+        }
+        // console.log(localStorage);
+      });
+
+    // test@testtest.co.kr
+    // testtest
+  };
+
+  return (
+    <div id={css['login']}>
+      <form className={css.cont}>
+        <div className={css.cnt_div}>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="이메일"
+            autoComplete="off"
+            onChange={handleInputsByName}
+          />
+        </div>
+        <div className={css.cnt_div}>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            autoComplete="off"
+            onChange={handleInputsByName}
+          />
+        </div>
+
+        <button className={css.btn_black} onClick={handleLogin}>
+          LOGIN
+        </button>
+        <p className={css.text}>
+          이메일, 비밀번호만 설정 후 회원가입 하시면 상품 결제, 주문 확인 및
+          배송 조회, 적립금 혜택 등 더욱 편리하게 스토어를 이용하실 수 있습니다.
+        </p>
+        <Link to="/signup" className={css.btn_goLink}>
+          회원가입 하기
+        </Link>
+        <button className={css.btn_goLink}>비밀번호 찾기</button>
+      </form>
+    </div>
   );
 }
 
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,42 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import css from './Signup.module.scss';
 
 function Signup() {
-  return (
-    <>
-      <Header />
-      <div id={css['signup']}>
-        <form className={css.cont}>
-          <div className={css.cnt_div}>
-            <label htmlFor={css.name}>이름</label>
-            <input
-              id="name"
-              type="text"
-              placeholder="이름"
-              autoComplete="off"
-            />
-          </div>
-          <div className={css.cnt_div}>
-            <label htmlFor="email">이메일</label>
-            <input
-              id="email"
-              type="text"
-              placeholder="이메일"
-              autoComplete="off"
-            />
-          </div>
+  const navigate = useNavigate();
 
-          <button className={css.btn_black}>CREATE ACCOUNT</button>
-          <Link to="/login" className={css.btn_goLink}>
-            로그인으로 돌아가기
-          </Link>
-        </form>
-      </div>
-      <Footer />
-    </>
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputsByName = e => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignUp = event => {
+    event.preventDefault();
+    fetch('http://localhost:10010/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: inputs.email,
+        password: inputs.password,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => console.log('결과: ', result));
+  };
+
+  return (
+    <div id={css['signup']}>
+      <form className={css.cont}>
+        <div className={css.cnt_div}>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="이메일"
+            autoComplete="off"
+            onChange={handleInputsByName}
+          />
+        </div>
+        <div className={css.cnt_div}>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            autoComplete="off"
+            onChange={handleInputsByName}
+          />
+        </div>
+
+        <button className={css.btn_black} onClick={handleSignUp}>
+          CREATE ACCOUNT
+        </button>
+        <Link to="/login" className={css.btn_goLink}>
+          로그인으로 돌아가기
+        </Link>
+      </form>
+    </div>
   );
 }
 
