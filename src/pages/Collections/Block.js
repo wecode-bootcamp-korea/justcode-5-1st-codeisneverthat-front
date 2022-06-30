0c5f8435327710f1a9915376890d5ea47c450b20
Bokyung Kim <kbkllmlk17@gmail.com>
2022-06-30 22:32:43 +0900

    image loading fixed final

--- src/pages/Collections/Block.js
+++ src/pages/Collections/Block.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Block.module.scss';
 import { Link } from 'react-router-dom';
 
@@ -7,13 +7,17 @@ function Block(props) {
   const link = '/product?id='.concat(id);
   console.log(id);
 
-  const [mainImage, setMainImage] = useState(props.image);
+  const [mainImage, setMainImage] = useState('');
+
+  useEffect(() => {
+    setMainImage(props.image);
+  }, [props.image]);
 
   return (
     <div className={css.block}>
       <Link to={link}>
         {' '}
-        <img className={css.image} src={props.image} />
+        <img className={css.image} src={mainImage} />
       </Link>
       <div className={css.subimages}>
         <img
