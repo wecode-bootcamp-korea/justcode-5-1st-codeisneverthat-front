4604bff02bf29cac1e1971192a0f366fc07ae817
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-07-01 12:21:00 +0900

    Fix: Top20 hover coloer 수정, Product detail slider arrow margin 수정, Home 이미지 수정 및 반응형 적용

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,82 +7,82 @@ function Home() {
     <div className={css.image_container}>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_1.png" />
+          <img src="https://images.unsplash.com/photo-1601044601940-f249b57ab584?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_2.png" />
+          <img src="https://images.unsplash.com/photo-1605905337381-cabe79fd2a38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_3.png" />
+          <img src="https://images.unsplash.com/photo-1594896980751-bed440689bae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_4.png" />
+          <img src="https://images.unsplash.com/photo-1589782051191-c338f9a283b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_5.png" />
+          <img src="https://images.unsplash.com/photo-1587538639197-cd3e6e9d120b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_6.png" />
+          <img src="https://images.unsplash.com/photo-1638178220878-f7d2da5f709b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_7.png" />
+          <img src="https://images.unsplash.com/photo-1598522396013-924ddcd44ada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_8.png" />
+          <img src="https://images.unsplash.com/photo-1598188014082-575f99169226?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_9.png" />
+          <img src="https://images.unsplash.com/photo-1599486970464-9689d3aea17a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_10.png" />
+          <img src="https://images.unsplash.com/photo-1594896980995-b7cb4ad9c496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_11.png" />
+          <img src="https://images.unsplash.com/photo-1621252610525-6c07df846017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1363&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_12.png" />
+          <img src="https://images.unsplash.com/photo-1623071279305-74260546bf66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_13.png" />
+          <img src="https://images.unsplash.com/photo-1605905337581-a039548358bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1353&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_14.png" />
+          <img src="https://images.unsplash.com/photo-1605885948690-27db84d97268?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_15.png" />
+          <img src="https://images.unsplash.com/photo-1590736661001-aa0632b14f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80" />
         </Link>
       </div>
       <div className={css.images}>
         <Link to="/collections">
-          <img src="https://cdn.shopify.com/s/files/1/0562/4971/2815/files/home220607_16.png" />
+          <img src="https://images.unsplash.com/photo-1594913784809-ac05f8491fdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
         </Link>
       </div>
       <Link className={css.maintext} to="/collections">
