c8c9fbfe1c1d5115d501c82305c21093c1c14987
sming0112 <sming0112@gmail.com>
2022-07-06 22:55:41 +0900

    Add: Image Detail Modal

--- src/pages/Product/modal/SizeFit.js
+++ src/pages/Product/modal/SizeFit.js
@@ -2,7 +2,7 @@ import React from 'react';
 import css from './SizeFit.module.scss';
 
 function SizeFit() {
-  return <></>;
+  return <div className={css.container}>사이즈 핏 모달</div>;
 }
 
 export default SizeFit;
