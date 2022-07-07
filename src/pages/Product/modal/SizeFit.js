d2db0185f59a6d5e23e1f7a8f2dc480b1194dba7
sming0112 <sming0112@gmail.com>
2022-07-08 00:35:44 +0900

    Add: Size&Fit Modal, Query Prams page

--- src/pages/Product/modal/SizeFit.js
+++ src/pages/Product/modal/SizeFit.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import css from './SizeFit.module.scss';
 
-function SizeFit() {
-  return <div className={css.container}>사이즈 핏 모달</div>;
+function SizeFit({ openSizeFitModal }) {
+  return (
+    <div className={css.container}>
+      <div className={css.sizeFitHeader}>
+        <div className={css.sizeFitHead}>PRODUCT MEASUREMENTS</div>
+        <div className={css.closeButton} onClick={openSizeFitModal}>
+          CLOSE
+        </div>
+      </div>
+      <div>
+        <img
+          className={css.sizeTable}
+          alt="사이즈표"
+          src="https://mblogthumb-phinf.pstatic.net/MjAxODA3MjlfMTYx/MDAxNTMyNzkwNzMwODk3.umnTfyqzGWCsxK1mbfuVe2lJ5INL7D1bh0iCKjBI7pMg.gCGZxb-RXcdI4twMuBmSPgoj4q60BHUACJr3BldkHYQg.JPEG.p1_____/output_3759994961.jpg?type=w800"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default SizeFit;
