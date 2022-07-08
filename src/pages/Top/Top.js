8668e7a3d63bfad4173827a24a21016d6e7bea88
Bokyung Kim <kbkllmlk17@gmail.com>
2022-07-08 16:17:13 +0900

    날짜 고침

--- src/pages/Top/Top.js
+++ src/pages/Top/Top.js
@@ -18,8 +18,53 @@ function Top() {
   const [month, setMonth] = useState(current.getMonth() + 1);
   const [year, setYear] = useState(current.getFullYear());
 
+  function numberToString(num) {
+    var result;
+    switch (num) {
+      case 1:
+        result = 'January';
+        break;
+      case 2:
+        result = 'February';
+        break;
+      case 3:
+        result = 'March';
+        break;
+      case 4:
+        result = 'April';
+        break;
+      case 5:
+        result = 'May';
+        break;
+      case 6:
+        result = 'June';
+        break;
+      case 7:
+        result = 'July';
+        break;
+      case 8:
+        result = 'August';
+        break;
+      case 9:
+        result = 'September';
+        break;
+      case 10:
+        result = 'October';
+        break;
+      case 11:
+        result = 'November';
+        break;
+      case 12:
+        result = 'December';
+        break;
+      default:
+        result = 'none';
+    }
+    return result;
+  }
+
   useEffect(() => {
-    setMonth(current.getMonth() + 1);
+    setMonth(numberToString(current.getMonth() + 1));
     setYear(current.getFullYear());
   }, [current]);
 
